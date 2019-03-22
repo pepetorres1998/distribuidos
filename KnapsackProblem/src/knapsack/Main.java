07d14862b18fb1674fe23693766c6277db6978ff
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2019-03-22 17:53:04 -0600

    changed number of iterations to 25

--- KnapsackProblem/src/knapsack/Main.java
+++ KnapsackProblem/src/knapsack/Main.java
@@ -4,12 +4,12 @@ public class Main {
 	public static void main(String[] args)
 	{
 		Generator g = new Generator(2000, 10000, 500, 15, 1, 0.005);
-		
-		for (int i = 1; i <= 50; i++) {
+
+		for (int i = 1; i <= 25; i++) {
 			long initialTime = System.currentTimeMillis();
 			Distribution dis = new Distribution(2000, i, g, 200);
-			System.out.println("Tiempo transcurrido: " + 
-					(System.currentTimeMillis() - initialTime) + 
+			System.out.println("Tiempo transcurrido: " +
+					(System.currentTimeMillis() - initialTime) +
 					" milisegundos");
 			System.out.println("Mejor resultado: " + dis.maxWeight + "\n");
 		}
